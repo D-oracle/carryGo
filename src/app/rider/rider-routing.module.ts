abda1d4301e6ea561b98b1936eb2a864416d4f3c
Godgift <dr.gggsquare@gmail.com>
2022-06-27 13:39:47 +0100

    *Adjusted Home Page

--- src/app/rider/rider-routing.module.ts
+++ src/app/rider/rider-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountsComponent } from './accounts/accounts.component';
+import { OrdersComponent } from './orders/orders.component';
+import { RiderComponent } from './rider.component';
+import { TransactionsComponent } from './transactions/transactions.component';
 
 const routes: Routes = [
-  // { path: '/wallet', component: W}
+  { path: '', component: RiderComponent,
+    children: [      
+      { path: 'transactions', component: TransactionsComponent },
+      { path: 'account', component: AccountsComponent },
+      { path: 'order', component: OrdersComponent }
+    ]
+  },
 ];
 
 @NgModule({
--- src/app/rider/rider.module.ts
+++ src/app/rider/rider.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RiderRoutingModule } from './rider-routing.module';
 import { RiderComponent } from './rider.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { OrdersComponent } from './orders/orders.component';
+import { DeliveredComponent } from './delivered/delivered.component';
+import { BottomNavComponent } from './bottom-nav/bottom-nav.component';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
 @NgModule({
   declarations: [
-    RiderComponent
+    RiderComponent,
+    TransactionsComponent,
+    AccountsComponent,
+    BottomNavComponent,
+    OrdersComponent,
+    DeliveredComponent
   ],
   imports: [
     CommonModule,
+    MatIconModule,
     RiderRoutingModule
   ]
 })
